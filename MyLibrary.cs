732f45b36dd81b8cc1f7710b48673585e20edb33
MichaelLake2015 <michaellake2011@gmail.com>
2015-08-20 15:51:17 +0700

    Component xử lý toán học
    Xử lý các

---
+++ MyLibrary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    public class MyClass
+    {
+        public void In()
+        {
+            Console.WriteLine("namespace A");
+        }
+    }
+
+    namespace A1
+    {
+         public class MyMath
+         {
+            public void In()
+             {
+            Console.WriteLine("namespace A1");
+             }
+         }
+    }
+}
+namespace B
+{
+    public class MyClass
+    {
+        public void In()
+        {
+            Console.WriteLine("namespace B");
+        }
+    }
+}
